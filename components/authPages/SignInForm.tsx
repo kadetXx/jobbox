07764a213fd61506c158aa51d86f51352e0f2582
kadet <techkadet@gmail.com>
2021-05-03 18:55:50 +0100

    added sign in page

--- components/authPages/SignInForm.tsx
+++ components/authPages/SignInForm.tsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React from "react";
 
-const SignIn = () => {
+import styles from "./AuthForms.module.scss";
+
+import Image from "next/image";
+
+import { useForm } from "react-hook-form";
+
+import { Input } from "@/shared";
+
+import { connect } from "react-redux";
+import { setUser } from "@/store/user/user.actions";
+
+const SignIn = ({ onSubmit, user }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className={styles.form}>
+      <div className={styles.form_header}>
+        <h3 className={styles.form_title}> Hello 👋🏼 </h3>
+        <p className={styles.form_subtitle}>Sign in to your Jobbox account</p>
+      </div>
 
-export default SignIn
+      <form className={styles.form_fields} onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          type="email"
+          placeholder="Email Address"
+          errors={errors}
+          register={register("email", { required: true })}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          errors={errors}
+          register={register("password", { required: true })}
+        />
+        <div className={styles.form_submit}>
+          <button className={`btn btn__primary btn__100 ${styles.btn}`}>
+            Sign In
+          </button>
+          <button className={`btn btn__white btn__100 ${styles.btn}`}>
+            <i className="btn_icon btn_icon__prefix">
+              <Image src="/img/google_logo.webp" width="18" height="18" />
+            </i>
+            Sign In WIth Google
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+const mapStateToProps = (state: any) => ({
+  user: state.user,
+});
+
+export default connect(mapStateToProps)(SignIn);
--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -2,10 +2,16 @@ import React from 'react'
 
 import { AuthLayout as Layout } from '@/layouts'
 
+import { SignInForm } from '@/components/authPages'
+
 const signIn = () => {
+  const signIn = () => {
+
+  }
+  
   return (
     <Layout>
-      
+      <SignInForm onSubmit={signIn} />
     </Layout>
   )
 }
