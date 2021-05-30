336a30a5e846e02ee0fc28c17a2335318c16fe7b
kadet <techkadet@gmail.com>
2021-05-30 23:49:43 +0100

    saving changes before gsap three

--- components/comingSoon/pictureStack/PictureStack.tsx
+++ components/comingSoon/pictureStack/PictureStack.tsx
@@ -28,7 +28,7 @@ const images = [
 
 const PictureStack = () => {
   const imageContainer = useRef(null);
-  const paperclip = useRef(null)
+  const paperclip = useRef(null);
   const onScreen = useIntersecting(imageContainer, 0.5);
 
   useEffect(() => {
@@ -41,6 +41,10 @@ const PictureStack = () => {
       return imageContainer.current.children[i + 1];
     });
 
+    const allImageBoxes = images.map((item, i) => {
+      return imageContainer.current.children[i].lastElementChild.children[0]
+    });
+
     if (onScreen) {
       TweenLite.to([...allItems], 0.1, {
         opacity: 1,
@@ -50,17 +54,23 @@ const PictureStack = () => {
 
       TweenLite.from([...allItems], 1, {
         x: "-220%",
-        scale: 2,
+        scale: 1.2,
         ease: Power3.easeIn,
         stagger: 0.4,
       });
+
+      TweenLite.from([...allImageBoxes], 2, {
+        scale: 1.3,
+        ease: Power3.easeOut,
+        stagger: 0.4,
+      });
+
     } else {
       TweenLite.to([...allItems], 0.1, {
         opacity: 0,
         ease: Power3.easeIn,
       });
     }
-
   }, [onScreen]);
 
   return (
@@ -80,7 +90,9 @@ const PictureStack = () => {
               <Image src="/svg/paperclip.svg" width="64" height="64" />
             </div>
           )}
-          <Image src={item.src} width="425.87" height="534" />
+          <div className={styles.stack_image}>
+            <Image src={item.src} width="425.87" height="534" />
+          </div>
         </div>
       ))}
     </div>
--- components/comingSoon/scrollScreen/ScrollScreen.tsx
+++ components/comingSoon/scrollScreen/ScrollScreen.tsx
@@ -1,20 +1,82 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 import styles from "./ScrollScreen.module.scss";
 
 import { JobExhibitCard } from "@/shared";
 
+import Marquee from "react-fast-marquee";
+
+const jobs = [
+  {
+    title: "Senior Product Designer",
+    company: "BBC News",
+    image: "/svg/bbc.svg",
+    jobType: "Full time",
+    location: "Remote",
+  },
+
+  {
+    title: "Senior Product Designer",
+    company: "BBC News",
+    image: "/svg/float-eight.svg",
+    jobType: "Full time",
+    location: "Remote",
+  },
+
+  {
+    title: "Senior Product Designer",
+    company: "BBC News",
+    image: "/svg/bbc.svg",
+    jobType: "Full time",
+    location: "Remote",
+  },
+
+  {
+    title: "Customer Service",
+    company: "BBC News",
+    image: "/svg/float-five.svg",
+    jobType: "Full time",
+    location: "Remote",
+  },
+
+  {
+    title: "Senior Product Designer",
+    company: "BBC News",
+    image: "/svg/bbc.svg",
+    jobType: "Full time",
+    location: "Remote",
+  },
+];
+
 const ScrollScreen = () => {
+  const marqueeRef = useRef(null);
+
+  useEffect(() => {
+
+    setTimeout(() => {
+      marqueeRef !== null && (marqueeRef.current.children[0].id = 'marqueeId');
+
+      console.log(marqueeRef.current.children[0]);
+    }, 1000);
+    
+    
+  }, [marqueeRef])
+
   return (
     <div id={styles.scrollScreen}>
-      <div className={styles.scroller}>
-        <JobExhibitCard
-          title="Senior Product Designer"
-          company="BBC News"
-          image="/svg/bbc.svg"
-          jobType="Full time"
-          location="Remote"
-        />
+      <div className={styles.scroller} ref={marqueeRef}>
+        <Marquee className={styles.marquee} speed={50} >
+          {jobs.map((item, index) => (
+            <JobExhibitCard
+              key={item.company + index}
+              title={item.title}
+              company={item.company}
+              image={item.image}
+              jobType={item.jobType}
+              location={item.location}
+            />
+          ))}
+        </Marquee>
       </div>
     </div>
   );
