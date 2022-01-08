7a11a370f109f2a00028fad77c884e6fda1a7138
kadetXx <techkadet@gmail.com>
2022-01-08 23:59:35 +0100

    fixed build errors

--- shared/index.ts
+++ shared/index.ts
@@ -1,19 +1,9 @@
 import Logo from './logo/Logo';
 import Preloader from './preloader/Preloader'
-import Input from './input/Input'
-import Select from './inputSelect/InputSelect'
-import JobExhibitCard from './jobExhibitCard/JobExhibitCard'
-import Badge from './badge/Badge'
-import Alert from './alert/Alert'
 import Button from './button/Button';
 
 export {
   Logo,
   Preloader,
-  Input,
-  Select,
-  JobExhibitCard,
-  Badge,
-  Alert,
   Button
 }
